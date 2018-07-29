7d7ceeb36d136924665a73c6effde4e24f6af06f
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-07-28 21:54:36 -0400

    Add simple test for uiInit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,18 @@
 using Libui
 @static if VERSION >= v"0.7-"
-    using Test
+  using Test
 else
-    using Base.Test
-end
-@static if VERSION < v"0.7-"
-	using Compat: Nothing
+  using Base.Test
 end
 
 @testset "Init and Uninit" begin
-o = Libui.uiInitOptions(0)
-err = uiInit(Ref(o))
+  o = Libui.uiInitOptions(0)
+  err = uiInit(Ref(o))
+  @test err == C_NULL
 
-@show err
-@show o
+  uiUninit()
+end
 
-uiUninit()
+@testset "Control Gallery" begin
+  include("controlgallery.jl")
 end
